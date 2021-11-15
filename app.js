87c2d79cc473f69346241318e8a2e2e352578b85
Olivier02500 <olivier.peuchet@hotmail.com>
2021-11-15 10:22:22 +0100

    make exo 56

---
+++ app.js
@@ -0,0 +1,34 @@
+
+
+let item = prompt('accessoire informatique')
+console.log(item)
+
+switch (item) {
+    case 'souris':
+        alert("J'utilise une souris")
+        break;
+
+    case 'clavier':
+        alert("J'utilise un clavier")
+        break;
+
+    case 'ordinateur':
+        alert("J'utilise un ordinateur")
+        break;
+
+    case 'tapis':
+        alert("J'utilise un tapis")
+        break;
+
+    case 'imprimante':
+        alert("J'utilise une imprimante")
+        break;
+
+    case 'ondulateur':
+        alert("J'utilise un ondulateur")
+        break;
+
+    default :
+        alert("Votre accessoire est inconnu")
+}
+
